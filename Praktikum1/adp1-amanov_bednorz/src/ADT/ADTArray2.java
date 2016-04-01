b7beb38e36add4b1c5647a7ee12a4e0751a220ac
atabaman <shiroker@mail.ru>
2016-04-01 20:05:37 +0500

    ADTStack half modified

--- Praktikum1/adp1-amanov_bednorz/src/ADT/ADTArray2.java
+++ Praktikum1/adp1-amanov_bednorz/src/ADT/ADTArray2.java
@@ -5,28 +5,41 @@ package ADT;
  * This class represent the implementation of the ADT 'ADTArray2', which uses the Java type int[] as the internal representation of the data.
  */
 public class ADTArray2 {
+	
+	private int[] elements;
+	
 
     public static ADTArray2 initA(Integer size){
         return new ADTArray2(size);
     }
 
     private ADTArray2(Integer size){
+    	elements = new int[size];
 
     }
 
     public void setA(Integer pos, Integer elem){
+    	elements[pos] = elem;
 
     }
 
     public Integer getA(Integer pos){
-        return null;
+        return elements[pos];
     }
 
     public Integer lengthA(){
-        return 0;
+        return elements.length;
     }
 
-    public boolean equalA(ADTArray1 array){
+    public boolean equalA(ADTArray2 array){
+    	
+    	if(this.lengthA()!=array.lengthA()) return false;
+    	
+    	for(int i=0; i<=this.lengthA(); i++){
+    		
+    		if(this.elements[i] != array.getA(i)) return false;
+    	}
+    	
         return true;
     }
 }
--- Praktikum1/adp1-amanov_bednorz/src/ADT/ADTStack.java
+++ Praktikum1/adp1-amanov_bednorz/src/ADT/ADTStack.java
@@ -5,35 +5,50 @@ package ADT;
  * This class represent the implementation of the ADT 'ADTStack'.
  */
 public class ADTStack {
+	
 
     public static ADTStack createS(){
         return new ADTStack();
     }
 
-    private ADTArray1 elements;
-
+    private ADTArray2 elements;
+    private int counter;
+    
     private ADTStack(){
-        elements = ADTArray1.initA(10);
+        elements = ADTArray2.initA(10);
     }
-
+    
+    
     public void push(Integer elem){
-
+    	
+    	elements.setA(counter, elem);
+    	counter++;
+    
     }
 
     public void pop(){
+    	
+    	counter--;
 
     }
 
     public Integer top(){
-        Integer elem = null;
-        return elem;
+        
+    	return elements.getA(counter);
     }
 
     public boolean isEmpty(){
+    	
+    	if(counter == 0) return true;
         return false;
     }
 
     public boolean equalS(ADTStack stack){
+    	if(this.isEmpty() && stack.isEmpty()) return true;
+    	
+    	
+    	
+    	
         return true;
     }
 
